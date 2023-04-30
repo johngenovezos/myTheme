9261e92343d2f5596091ec343ab7759a9cd94ba7
John Genovezos <john@genovezos.com>
2023-05-01 01:12:01 +1000

    Update MyTheme.js

--- MyTheme.js
+++ MyTheme.js
@@ -1,4 +1,4 @@
-t.prefs_.set('color-palette-overrides',["#262626", "#bf97b4", "#8abe86", "#c0ae87", "#6889bf", "#c08867", "#6eb2bf", "#cdd6f4", "#adadad", "#f5c2e7", "#a6e3a1", "#f9e2af", "#89b4fa", "#fab387", "#89dceb", "#cdd6f4"]);
+t.prefs_.set('color-palette-overrides',["#262626", "#b18ca7", "#81b27d", "#b1a07c", "#6180b2", "#b27e5f", "#66a6b2", "#cdd6f4", "#adadad", "#f5c2e7", "#a6e3a1", "#f9e2af", "#89b4fa", "#fab387", "#89dceb", "#cdd6f4"]);
 t.prefs_.set('foreground-color', "#cdd6f4");
 t.prefs_.set('background-color', "#262626");
 t.prefs_.set('cursor-color', 'rgba(192,174,135,0.5)');
